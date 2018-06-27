68c52ea3f91a0940cc74fab7e7d7bf8e0c5066f4
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-27 01:36:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ if x.length == 0
 puts "The line is currently empty."
 else
 
-  x.each_with_index do |name, spot|
+  x.each_with_index(1) do |name, spot|
     puts "The line is currently: #{spot} #{name}."
 end
 end
