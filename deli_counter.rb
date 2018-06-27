3181550fe5fdcdf634397f8dcd8a9e315358b084
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-27 01:48:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,8 @@ end
 end
 
 def take_a_number(deli, name)
-  deli.push("#{name}")
-  puts "Welcome, #{name}. You are number 1 in line."
+  if deli.length == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+  else
+    "Welcome, #{name}. You are #{deli.length} in line."
 end
