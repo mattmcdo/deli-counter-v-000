d14949a6404b289763ffc96e1e0ced63deaf0aa6
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-06-27 01:44:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ puts "The line is currently empty."
 else
 message = "The line is currently:"
   x.each.with_index(1) do |name, index|
-deli << "#{message} #{index}. #{name}"
+deli.push("#{index}. #{name}")
 end
 end
 end
